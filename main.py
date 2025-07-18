8ed9063469ffaa5d300a1f1c02098b4bbe9127f6
Felix <bobir0263@gmail.com>
2025-07-18 13:19:54 +0300

    menu buttonini qo'shdik

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telebot import TeleBot
 from telebot.types import Message
+from buttons import menu
 
 TOKEN = '7738326967:AAHqHSaJXbpRfofEUN4aFpn8ZVYFlaEE76w'
 
@@ -9,7 +10,8 @@ bot = TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message: Message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, "Assalomu alekum !")
+    bot.send_message(chat_id, "Assalomu alekum !",
+                     reply_markup=menu())
 
 @bot.message_handler(commands=['help'])
 def help(message : Message):
