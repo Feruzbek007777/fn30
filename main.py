ced0860e9da9843ea36628127e7ad0dfcd767e39
Felix <bobir0263@gmail.com>
2025-07-18 12:53:46 +0300

    start handlarini qoshdik git add main.py !

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from telebot import TeleBot
+from telebot.types import Message
 
 TOKEN = '7738326967:AAHqHSaJXbpRfofEUN4aFpn8ZVYFlaEE76w'
 
 bot = TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message: Message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Assalomu alekum !")
+
 
 if __name__ == '__main__':
     bot.infinity_polling()
